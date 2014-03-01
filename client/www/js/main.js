500b7cd2a2e6ab7d4b7cd279f5aea257bc491121
PortWatcher <root@pwhack.me>
2014-03-01 17:45:31 +0800

    1. add fastclick 2. add IP input

--- client/www/js/main.js
+++ client/www/js/main.js
@@ -5,34 +5,12 @@
  * Time: 下午11:18
  */
 
+window.addEventListener('load', function () {
+	FastClick.attach(document.body);
+}, false);
+
 document.addEventListener('deviceready', function () {
-
-	Socket = new WebSocket('ws://' + settings.SERVER_ADDR + ':18080');
-
-	Socket.onopen = function () {
-		Socket.send(JSON.stringify({
-			packetType: settings.PACKET_TYPE.PACKET_SYSTEM,
-			dataType: settings.DATA_TYPE.CLIENT_AUTH,
-			message: {
-				role: settings.ROLE.PLAYER
-			}
-		}));
-
-		$('#status').html('已连接至服务器，等待对手加入');
-	};
-
-	Socket.onmessage = function (event) {
-		Dispatcher.dispatch(JSON.parse(event.data));
-	};
-
-	Socket.onerror = function () {
-		console.log('socket error');
-	};
-
-	Socket.onclose = function () {
-		$('#status').html('连接已断开');
-		console.log('socket closed');
-	};
+	$('#server_addr').val(settings.SERVER_ADDR);
 
 	$('#btnReady').on('click', function () {
 		// tell the server that the player is ready for battle
@@ -42,6 +20,38 @@ document.addEventListener('deviceready', function () {
 		}));
 
 		$('#status').html('已准备，等待对手');
+	});
+
+	$('#btnConnect').on('click', function () {
+		$('#btnConnect').hide();
+
+		var serverIP = $('#server_addr').val() || settings.SERVER_ADDR;
+		Socket = new WebSocket('ws://' + serverIP + ':18080');
+
+		Socket.onopen = function () {
+			Socket.send(JSON.stringify({
+				packetType: settings.PACKET_TYPE.PACKET_SYSTEM,
+				dataType: settings.DATA_TYPE.CLIENT_AUTH,
+				message: {
+					role: settings.ROLE.PLAYER
+				}
+			}));
+
+			$('#status').html('已连接至服务器，等待对手加入');
+		};
+
+		Socket.onmessage = function (event) {
+			Dispatcher.dispatch(JSON.parse(event.data));
+		};
+
+		Socket.onerror = function () {
+			console.log('socket error');
+		};
+
+		Socket.onclose = function () {
+			$('#status').html('连接已断开');
+			console.log('socket closed');
+		};
 	});
 });
 
